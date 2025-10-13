a1222e8ab7957926219e26a1c1deb1be579a24ca
Mads <ayeme@appstate.edu>
2025-10-12 21:41:49 -0700

    Update and rename Wolf.java to Pig.java

---
+++ Animals/Pig.java
@@ -0,0 +1,19 @@
+package animals;
+
+public abstract class Pig extends Animal {
+    public Pig(String name){
+        super(name);
+
+    }
+    public void performEat(){
+        System.out.println("Trough slop...");
+    }
+    public void performSound(){
+        System.out.println("oink...");
+    }
+    public void performMove()
+    {
+        System.out.println("Waddle...");
+    }
+} 
+
