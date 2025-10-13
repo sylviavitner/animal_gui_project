24020385e635a58728b08b0379bb89363eaeafa2
Mads <ayeme@appstate.edu>
2025-10-12 20:47:29 -0700

    Update Canine.java
    changed methods for the udpated ones.

--- Animals/Canine.java
+++ Animals/Canine.java
@@ -1,13 +1,24 @@
 package animals;
 
 public abstract class Canine extends Animal {
-    public Canine(String name){
+    public Canine (String name)
+    {
         super(name);
 
     }
-    public void roam(){
-        System.out.println("like canines roam in packs...");
-        hungerLevel = hungerLevel + 1; 
+    public void performMove()
+    {
+        System.out.println("Prowling...");
+    }
+    public void performSound()
+    {
+        System.out.println("Howling"...);
+    }
+
+    public void performEat()
+    {
+        System.out.println("Eating meat...");
     }
     
 }
+
