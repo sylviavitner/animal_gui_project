b944261b98fb4f99895dbc06a05577dbfada5322
Mads <ayeme@appstate.edu>
2025-10-12 21:13:36 -0700

    Update and rename Canine.java to Snake.java
    made it a concrete class, changed to snake

---
+++ Animals/Snake.java
@@ -0,0 +1,25 @@
+package animals;
+
+public class Snake extends Animal {
+    public Snake (String name)
+    {
+        super(name);
+
+    }
+    public void performMove()
+    {
+        System.out.println("Slithering...");
+    }
+    public void performSound()
+    {
+        System.out.println("Hissss"...);
+    }
+
+    public void performEat()
+    {
+        System.out.println("Swallows food whole...");
+    }
+    
+}
+
+
