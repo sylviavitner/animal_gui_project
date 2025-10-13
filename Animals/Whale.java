ca30dd3d2d9d246922b9feaeb0a8958f27a1ddcc
Mads <ayeme@appstate.edu>
2025-10-12 21:31:11 -0700

    Update and rename Hippo.java to Whale.java

---
+++ Animals/Whale.java
@@ -0,0 +1,23 @@
+package animals;
+
+public abstract class Whale extends Animal {
+    public Whale(String name)
+    {
+        super(name);
+    }
+    public void performMove()
+    {
+        System.out.println("Swimming...");
+    }
+    public void performSound()
+    {
+        System.out.println("Awoo...");
+    }
+    public void performEat()
+    {
+        System.out.println("Eating plankton...");
+    }
+    
+}
+
+ 
