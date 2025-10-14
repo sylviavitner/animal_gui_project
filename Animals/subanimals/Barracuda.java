6298963a2d2c381cb3ee1e6aa3f40ba8e4a4a093
Mads <ayeme@appstate.edu>
2025-10-14 14:30:10 -0400

    Update Barracuda.java
    implemented loadanimalwindow

--- Animals/subanimals/Barracuda.java
+++ Animals/subanimals/Barracuda.java
@@ -1,1 +1,34 @@
+package Animals.subanimals;
 
+public class Barracuda extends Animals.Fish{
+    private static String[] buttonType = {"Sound","Eat", "Move", "WIKIPEDIAAAAAAAA"};
+    private static String[] actions = {"Dun dun dun du...", "chomp...", "Races in the water..."};
+    public Barracuda (String name){
+        super(name,buttonType);
+    }
+	
+    public void loadAnimalWindow(String name) {
+        //create a new instance of the AnimalWindow class
+        switch(name) {
+            case "Sound":
+                perform(actions[0]);
+                break;
+            case "Eat":
+                perform(actions[1]);
+                break;
+            case "Move":
+                perform(actions[2]);
+                break;
+            case "WIKIPEDIAAAAAAAA":
+                String url = "https://en.wikipedia.org/wiki/Barracuda";
+                try {
+                    java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+                } catch (java.io.IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Unknown fish type: " + name);
+                break;
+        }
+    }
