56c0446af0e5c06861765331824bd18e427fae07
Mads <ayeme@appstate.edu>
2025-10-14 14:10:47 -0400

    Update Swallowtail.java
    Added loadanimal window implementation

--- Animals/subanimals/Swallowtail.java
+++ Animals/subanimals/Swallowtail.java
@@ -1,7 +1,34 @@
 package Animals.subanimals;
 
-public class Swallowtail extends Animals.Butterfly {
-    public Swallowtail(String name){
-        super(name);
+public class Monarch extends Animals.Butterfly{
+    private static String[] buttonType = {"Sound","Eat", "Move", "WIKIPEDIAAAAAAAA"};
+    private static String[] actions = {"ps ps ps...", "Drinking Thistle...", "flap flap flap...",};
+    public Monarch(String name){
+        super(name,buttonType);
     }
-}
+	
+    public void loadAnimalWindow(String name) {
+        //create a new instance of the AnimalWindow class
+        switch(name) {
+            case "Sound":
+                perform(actions[0]);
+                break;
+            case "Eat":
+                perform(actions[1]);
+                break;
+            case "Move":
+                perform(actions[2]);
+                break;
+            case "WIKIPEDIAAAAAAAA":
+                String url = "https://en.wikipedia.org/wiki/Swallowtail_butterfly";
+                try {
+                    java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+                } catch (java.io.IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Unknown butterfly type: " + name);
+                break;
+        }
+    }
