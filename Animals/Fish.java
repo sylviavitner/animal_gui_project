4287eaa02e996f87fdfed8b86e28068c0bec8909
Mads <ayeme@appstate.edu>
2025-10-14 18:19:38 -0400

    Update Fish.java
    Added bracket and Alligator Gar

--- Animals/Fish.java
+++ Animals/Fish.java
@@ -3,9 +3,8 @@ package Animals;
 import javax.swing.JLabel;
 
 
-
 public abstract class Fish extends Animal {
-    static String[] buttonType = {"Barracuda","Swallowtail"};
+    static String[] buttonType = {"Barracuda","AlligatorGar"};
     public Fish(String name){
         super(name,buttonType);
     }
@@ -18,14 +17,15 @@ public abstract class Fish extends Animal {
         displayFrame();
 
     }
-    public void loadAnimalWindow(String name) {
+    public void loadAnimalWindow(String name) 
+    {
         //create a new instance of the AnimalWindow class
         switch(name) {
             case "Barracuda":
                 new Barracuda(name);
                 break;
-            case "Swallowtail":
-                new Swallowtail(name);
+            case "AlligatorGar":
+                new AlligatorGar(name);
             default:
                 System.out.println("Unknown Fish type: " + name);
                 break;
@@ -35,16 +35,12 @@ public abstract class Fish extends Animal {
         JLabel label = new JLabel(action);
         frame2.add(label);
     }
-    
-    public class Barracuda extends Animals.Fish
-    {
+    public class Barracuda extends Fish{
         private static String[] buttonType = {"Sound","Eat", "Move", "WIKIPEDIAAAAAAAA"};
-        private static String[] actions = {"Dun dun dun du...", "chomp...", "Races in the water..."};
-        public Barracuda (String name)
-        {
+        private static String[] actions = {"fwsh fwsh fwsh...","Lurking in the water...","Chomp..."};
+        public Barracuda(String name){
             super(name,buttonType);
         }
-    }
         
         public void loadAnimalWindow(String name) {
             //create a new instance of the AnimalWindow class
@@ -53,10 +49,10 @@ public abstract class Fish extends Animal {
                     perform(actions[0]);
                     break;
                 case "Eat":
-                    perform(actions[1]);
+                    perform(actions[2]);
                     break;
                 case "Move":
-                    perform(actions[2]);
+                    perform(actions[1]);
                     break;
                 case "WIKIPEDIAAAAAAAA":
                     String url = "https://en.wikipedia.org/wiki/Barracuda";
@@ -67,10 +63,47 @@ public abstract class Fish extends Animal {
                     }
                     break;
                 default:
-                    System.out.println("Unknown fish type: " + name);
+                    System.out.println("Unknown Fish type: " + name);
+                    break;
+            }
+        }
+
+
+    }
+    public class AlligatorGar extends Animals.Fish{
+        private static String[] buttonType = {"Sound","Eat", "Move", "WIKIPEDIAAAAAAAA"};
+        private static String[] actions = {"See you later...", "swim swim swim...", "Tear through flesh...",};
+        public AlligatorGar(String name){
+            super(name,buttonType);
+        }
+        
+        public void loadAnimalWindow(String name) {
+            //create a new instance of the AnimalWindow class
+            switch(name) 
+            {
+                case "Sound":
+                    perform(actions[0]);
+                    break;
+                case "Eat":
+                    perform(actions[1]);
+                    break;
+                case "Move":
+                    perform(actions[2]);
+                    break;
+                case "WIKIPEDIAAAAAAAA":
+                    String url = "https://en.wikipedia.org/wiki/Alligator_gar";
+                    try {
+                        java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+                    } catch (java.io.IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                default:
+                    System.out.println("Unknown Fish type: " + name);
                     break;
             }
         }
     }
-
+    
 }
+
